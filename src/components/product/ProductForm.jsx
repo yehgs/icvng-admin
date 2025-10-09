a72d252f6bd5602e559a56c48423d6c8fd03052e
Calstins <yehgs.co.uk@gmail.com>
2025-10-09 10:16:03 +0100

    added kg inidicator and update flutterwave to paystack

--- src/components/product/ProductForm.jsx
+++ src/components/product/ProductForm.jsx
@@ -401,7 +401,7 @@ const ProductForm = ({ isOpen, onClose, product = null, onSuccess }) => {
 
                 <div>
                   <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
-                    Weight
+                    Weight (kg)
                   </label>
                   <input
                     type="number"
